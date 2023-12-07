b244ac9ceb5ce2767b04bea1120bf9fbf08890d2
Lars Andersen <152974148+spoils-plunks@users.noreply.github.com>
2023-12-07 11:41:19 +0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    private String name;
+
+    public Customer(String customerName) {
+        this.name = customerName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
